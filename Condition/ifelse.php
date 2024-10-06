1389a9e92023ccf94622a3f70c29358fca2ca3b7
upida <salmamufida34@gmail.com>
2024-10-06 22:41:08 +0700

    condition

---
+++ Condition/ifelse.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+if ($a > $b) {
+    echo "a is greater than b";
+} else {
+    echo "b is greater than a";
+}
---
+++ Condition/switch.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 10;
+
+switch ($a) {
+    case 10:
+        echo "a is 10";
+        break;
+    case 20:
+        echo "a is 20";
+        break;
+    default:
+        echo "a is not 10 or 20";
+        break;
+}
